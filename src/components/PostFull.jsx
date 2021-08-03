22a4bc971a119f85be45678419a2fdcb411420fe
Leandro Bezerra <leandrokw@gmail.com>
2021-08-03 11:44:30 -0300

    Page Post - add component PostFull and styled

---
+++ src/components/PostFull.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from '../assets/styles/posts.module.css';
+
+export default function PostFull({
+	id,
+	image,
+	slug,
+	path,
+	date,
+	title,
+	description,
+	content,
+	user,
+	tags,
+	reactionsCount,
+	commentsCount,
+	readTime,
+}) {
+	return (
+		<div className="row">
+			<div className="col-1">
+				<ul className={styles.post__side}>
+					<li>
+						<i className="far fa-heart"></i>
+						{reactionsCount}
+					</li>
+					<li>
+						<i className="far fa-comment fa-flip-horizontal"></i>
+						{commentsCount}
+					</li>
+				</ul>
+			</div>
+
+			<div className="col-11">
+				<article className={styles.post}>
+					{image && (
+						<div className={styles.image__featured}>
+							<img src={image} className="img-fluid" alt={title} />
+						</div>
+					)}
+
+					<div className="p-3 px-md-5 py-md-4">
+						<div className="row d-flex align-items-center mb-4">
+							<div className="col-2">
+								<img src={user?.profile_image_90} className="img-fluid" alt={user?.name} />
+							</div>
+							<div className="col">
+								By <strong>{user?.name}</strong> in <strong>{date}</strong> - {readTime} min read
+							</div>
+						</div>
+
+						<hgroup>
+							<h2 className={styles.title}>{title}</h2>
+							<h3 className={styles.subtitle}>{description}</h3>
+						</hgroup>
+
+						{tags && (
+							<div className={styles.tags}>
+								{tags.map((tag) => (
+									<a key={tag} href={`https://dev.to/t/${tag}`} target="_blank">
+										<span>#</span> {tag}
+									</a>
+								))}
+							</div>
+						)}
+
+						<hr />
+
+						<div className="row">
+							<div className="col">{user?.name}</div>
+							<div className="col">{date}</div>
+						</div>
+
+						<div className={styles.content}>
+							<div dangerouslySetInnerHTML={{ __html: content }} />
+						</div>
+					</div>
+				</article>
+			</div>
+		</div>
+	);
+}
+
+PostFull.propTypes = {
+	id: PropTypes.number,
+	image: PropTypes.string,
+	slug: PropTypes.string,
+	path: PropTypes.string,
+	date: PropTypes.string,
+	description: PropTypes.string,
+	content: PropTypes.string,
+	user: PropTypes.object,
+	tags: PropTypes.array,
+	reactionsCount: PropTypes.number,
+	commentsCount: PropTypes.number,
+	readTime: PropTypes.number,
+};
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { StoreContext } from '../contexts/StoreContext';
+
+import LayoutBase from '../components/LayoutBase';
+import PostFull from '../components/PostFull';
 
 export default function Post() {
-	return <>Post</>;
+	const { getPost, post, loading, failed } = React.useContext(StoreContext);
+
+	const location = useLocation();
+
+	React.useEffect(() => {
+		getPost(location.pathname.replace('/post/', ''));
+	}, [location]);
+
+	console.log(post);
+
+	return (
+		<LayoutBase isSidebar={false}>
+			{loading ? (
+				<div className="alert alert-info">Loading post...</div>
+			) : failed ? (
+				<div className="alert alert-danger">An error occurred</div>
+			) : (
+				<PostFull
+					key={post.id}
+					id={post.id}
+					image={post.cover_image}
+					slug={post.slug}
+					path={post.path}
+					date={post.readable_publish_date}
+					title={post.title}
+					description={post.description}
+					content={post.body_html}
+					tags={post.tags}
+					user={post.user}
+					reactionsCount={post.comments_count}
+					commentsCount={post.public_reactions_count}
+					readTime={post.reading_time_minutes}
+				/>
+			)}
+		</LayoutBase>
+	);
 }

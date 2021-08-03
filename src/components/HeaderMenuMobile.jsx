68cc69aa0bf926861bc9dc90b02ab11020c54e52
Leandro Bezerra <leandrokw@gmail.com>
2021-08-03 15:44:40 -0300

    Component HeaderMenuMobile - change class icon to className

--- src/components/HeaderMenuMobile.jsx
+++ src/components/HeaderMenuMobile.jsx
@@ -6,7 +6,7 @@ export default function HeaderMenuMobile() {
 	return (
 		<div className={`${styles.menu__mobile} d-inline d-md-none`}>
 			<button type="button" className={`btn btn-link ${styles.btn__menu} font-bold me-2`}>
-				<i class="fas fa-bars"></i>
+				<i className="fas fa-bars"></i>
 			</button>
 		</div>
 	);
